2c74ae4d8dc5eb562c6f3f37c0cad597b00191ba
yakube <44039980+yakube@users.noreply.github.com>
2018-10-10 16:16:59 -0500

    Add files via upload

---
+++ test/test.js
@@ -0,0 +1,15 @@
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(random(255),random(255),random(255));
+}
+function mouseClicked()
+{
+}
+function windowResized()
+{
+  createCanvas(windowWidth, windowHeight);
+}
