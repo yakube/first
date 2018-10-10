c151c11c01824b4f0c2fc7096852246a4956acdc
yakube <44039980+yakube@users.noreply.github.com>
2018-10-10 16:48:58 -0500

    Add files via upload

--- test/test.js
+++ test/test.js
@@ -1,18 +1,26 @@
-
+var doit;
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  doit=false;
 }
 
 function draw() {
-  if(mouseIsPressed)
-    background(random(255));
-  //background(0);
-  color(255);
+  if(doit)
+    background(random(255),random(255),random(255));
+  else
+    background(0);
+  fill(255,0,0);
   rect(50,50,50,50);
+  textSize(30);
+  text("Haha",50,50);
 }
 function mousePressed()
 {
-  //background(random(255));
+  doit=true;
+}
+function mouseReleased()
+{
+  doit=false;
 }
 function windowResized()
 {
